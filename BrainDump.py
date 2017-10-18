6c2d3f3fc8b5df91df0094012d4b50081bac9aef
kwi5i <32113899+kiwi5@users.noreply.github.com>
2017-10-18 21:18:14 +0200

    crude functions incoming.

--- BrainDump.py
+++ BrainDump.py
@@ -5,38 +5,55 @@
 # interPend()
 # goodOnes()
 
-#=======================================
-createFile(date.time())
+# ======================================================================
+def createFile(date.time()):
 # opening session generates file
-#  file of a session is named according to the date & time created
+   # file of a session is named according to the date & time created
 
-firstLine("time.begin - time.end | session.duration:")
+def firstLine("time.begin - time.end | session.duration:"):
 # first line of a file contains date of beginning
-#  and at the end is modified: maybe put there some unique str, and replace it?
-#  as well as summation of how long the session has taken, and nr of entries
+   # and at the end is modified: maybe put there some unique str, and replace it?
+   lineAppend(date() +" "+ time() +"endTime-placeholder")
+   #TODO: +endDate if session across the midnight
+   #BONUS: as well as summation of how long the session has taken, and nr of entries
 
-newEntry()
+def newEntry():
 # displays an empty field for new text input
-#  NOTHING previous is visible
-#  display instructions/shortcuts at the top of field
-#  write in the middle of field (or x lines below instructions)
+   #TODO: put this in infinite loop
+   eraseField()
+   # display instructions/shortcuts at the top of field
+   print("Newline: Shift-Enter   Commit: Enter   Escape: write 'End'")
+   print()
+   print()
+   print()
+   print("What's on Your mind, now?")
+   # write in the middle of field (or x lines below instructions)
+   line = input(" >>  ")
+   lineAppend(line)
 
-lineAppend()
+def lineAppend():
 # writes each entry to a LINE of a file
-#  newlines are replaced w/ "|"s
-#  S-Enter == newline ?
+   # BONUS: newlines are replaced w/ "|"s  &&  S-Enter == newline
 
-#=======================================
-interPend()
+# ======================================================================
+def interPend():
 #BONUS: new entry w/o submitting current one
-#  it is shown again afterwards
-#  simple shortcut to do it QUICKLY
-#  empty line is left where this'd be, and entry is prepended with nr of that line
+   # it is shown again afterwards
+   # simple shortcut to do it QUICKLY
+   # empty line is left where this'd be, and entry is prepended with nr of that line
 
-goodOnes()
+def goodOnes():
 #BONUS: after closing session: option to view contents line-by-line, and mark them as 'good ideas'
-#  WORKING: open copied file in N++ > rm lines you don't like using C-S-l
-#  reacts to (displayed) keys
-#  creates new file out of chosen entries - old name appended w/ "chosen" etc.
+   # WORKING: open copied file in N++ > rm lines you don't like using C-S-l
+   # reacts to (displayed) keys
+   # creates new file out of chosen entries - old name appended w/ "chosen" etc.
 
 #BONUS: font size regulation
+
+# =============================== CODE: ================================
+createFile()
+firstLine()
+while True:
+   newEntry()
+   if lastLine == "End":
+      return
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # brain-dump
+
+### This is still "oh, so not working".
+Don't try to run this.
